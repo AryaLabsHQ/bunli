fcde8d0a1eb620c59f777f53f41767e570e4f243
Saatvik Arya <aryasaatvik@gmail.com>
2025-12-07 14:13:17 +0530

    chore(deps): update nextjs and fumadocs

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,5 +1,5 @@
 import './global.css';
-import { RootProvider } from 'fumadocs-ui/provider';
+import { RootProvider } from 'fumadocs-ui/provider/next';
 import { GeistSans } from 'geist/font/sans';
 import { GeistMono } from 'geist/font/mono';
 import type { ReactNode } from 'react';
